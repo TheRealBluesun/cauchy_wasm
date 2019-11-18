5e0c6714cc587dfdce39555ba34753e6acc74c5a
Chris Novick <c.r.novick@gmail.com>
2019-11-17 21:02:54 -0500

    Dev snapshot

--- src/cwasm/mod.rs
+++ src/cwasm/mod.rs
@@ -5,51 +5,77 @@ use std::io::{prelude::*, Cursor};
 
 #[derive(Debug)]
 enum SectionID {
-    Custom_Section = 0,
-    Type_Section = 1,
-    Import_Section = 2,
-    Function_Section = 3,
-    Table_Section = 4,
-    Memory_Section = 5,
-    Global_Section = 6,
-    Export_Section = 7,
-    Start_Section = 8,
-    Element_Section = 9,
-    Code_Section = 10,
-    Data_Section = 11,
-    Invalid_Section,
+    CustomSection = 0,
+    TypeSection = 1,
+    ImportSection = 2,
+    FunctionSection = 3,
+    TableSection = 4,
+    MemorySection = 5,
+    GlobalSection = 6,
+    ExportSection = 7,
+    StartSection = 8,
+    ElementSection = 9,
+    CodeSection = 10,
+    DataSection = 11,
+    InvalidSection,
 }
 
 impl SectionID {
     fn from_u8(value: u8) -> SectionID {
         match value {
-            0 => SectionID::Custom_Section,
-            1 => SectionID::Type_Section,
-            2 => SectionID::Import_Section,
-            3 => SectionID::Function_Section,
-            4 => SectionID::Table_Section,
-            5 => SectionID::Memory_Section,
-            6 => SectionID::Global_Section,
-            7 => SectionID::Export_Section,
-            8 => SectionID::Start_Section,
-            9 => SectionID::Element_Section,
-            10 => SectionID::Code_Section,
-            11 => SectionID::Data_Section,
-            _ => SectionID::Invalid_Section,
+            0 => SectionID::CustomSection,
+            1 => SectionID::TypeSection,
+            2 => SectionID::ImportSection,
+            3 => SectionID::FunctionSection,
+            4 => SectionID::TableSection,
+            5 => SectionID::MemorySection,
+            6 => SectionID::GlobalSection,
+            7 => SectionID::ExportSection,
+            8 => SectionID::StartSection,
+            9 => SectionID::ElementSection,
+            10 => SectionID::CodeSection,
+            11 => SectionID::DataSection,
+            _ => SectionID::InvalidSection,
         }
     }
 }
 
-enum ValueType {}
+#[derive(Debug)]
+enum ValueType {
+    F64 = 0x7C,
+    F32 = 0x7D,
+    I64 = 0x7E,
+    I32 = 0x7F,
+    Invalid,
+}
 
-pub struct CWasm {}
+impl ValueType {
+    fn from_u8(value: u8) -> ValueType {
+        match value {
+            0x7C => ValueType::F64,
+            0x7D => ValueType::F32,
+            0x7E => ValueType::I64,
+            0x7F => ValueType::I32,
+            _ => ValueType::Invalid,
+        }
+    }
+}
+
+struct Function {
+    params: Vec<ValueType>,
+    returns: Vec<ValueType>,
+}
+
+pub struct CWasm {
+    functions: Vec<Function>,
+}
 
 impl CWasm {
-    pub fn parse_wasm(binfile: &[u8]) {
+    pub fn parse_wasm(binfile: &[u8]) -> CWasm {
         let mut cur = Cursor::new(binfile);
         // First read the magic bytes
         let mut magic = [0u8; 4];
-        cur.read(&mut magic).expect("Could not read magic");
+        cur.read_exact(&mut magic).expect("Could not read magic");
         assert_eq!(&magic, b"\0asm");
         // // Then read the version
         let version = cur
@@ -60,7 +86,8 @@ impl CWasm {
             let id = SectionID::from_u8(id_byte);
             println!("found section {:?} ({:X})", id, id_byte);
             match id {
-                SectionID::Type_Section => CWasm::parse_section_type(&mut cur),
+                SectionID::TypeSection => CWasm::parse_section_type(&mut cur),
+                SectionID::FunctionSection => CWasm::parse_section_function(&mut cur),
                 _ => {
                     println!("No method to parse section {:?}", id);
                     break;
@@ -73,6 +100,9 @@ impl CWasm {
         cur.read_to_end(&mut remaining)
             .expect("Could not read unparsed data");
         println!("Buff left: {}", hex::encode(remaining));
+        CWasm {
+            functions: Vec::new(),
+        }
     }
 
     // The type section has the id 1. It decodes into a vector of function types
@@ -83,47 +113,35 @@ impl CWasm {
         let num_functions =
             leb128::read::unsigned(cur).expect("could not get numfuncs in Type section");
         println!("\tsection Type contains {} functions", num_functions);
+        let mut functions = Vec::<Function>::with_capacity(num_functions as usize);
         for _ in 0..num_functions {
             let func_byte = cur.read_u8().unwrap();
             if func_byte == 0x60 {
                 let num_params = leb128::read::unsigned(cur).expect("could not get param num");
+                let mut params = Vec::<ValueType>::with_capacity(num_params as usize);
                 for _ in 0..num_params {
                     let param_type = cur.read_u8().unwrap();
-                    println!("\t\tparam of type {:X}", param_type);
+                    params.push(ValueType::from_u8(param_type));
+                    println!("\t\tparam of type {:?} ({:X})", &params.last().unwrap(), param_type);
                 }
                 let num_results = leb128::read::unsigned(cur).expect("could not get result num");
+                let mut returns = Vec::<ValueType>::with_capacity(num_results as usize);
                 for _ in 0..num_results {
                     let resul_type = cur.read_u8().unwrap();
-                    println!("\t\treturn of type {:X}", resul_type);
+                    returns.push(ValueType::from_u8(resul_type));
+                    println!("\t\treturn of type {:?} ({:X})", &params.last().unwrap(), resul_type);
                 }
+                let func = Function {
+                    params,
+                    returns
+                };
+                functions.push(func);
             } else {
                 println!("Encountered invalid func byte {}", func_byte);
                 break;
             }
         }
-        // Function types are encoded by the byte 0x60 followed by the
-        // respective vectors of parameter and result types
-
-        // if let Some(mut buf) = CWasm::parse_vector(cur) {
-        //     println!("contents of section Type: {}", hex::encode(&buf));
-        //     // buf now contains the vector of function types
-        //     let num_functions = leb128::read::unsigned(&buf);
-        // } else {
-        //     println!("Unable to parse section Type");
-        // }
     }
 
-    fn parse_vector(cur: &mut Cursor<&[u8]>) -> Option<Vec<u8>> {
-        let size = leb128::read::unsigned(cur).expect("Could not decode vector size") as usize;
-        let mut buf = Vec::with_capacity(size);
-        let bytes_read = cur
-            .take(size as u64)
-            .read_to_end(&mut buf)
-            .expect("unable to read vector data");
-        if bytes_read == size {
-            Some(buf)
-        } else {
-            None
-        }
-    }
+    fn parse_section_function(cur: &mut Cursor<&[u8]>) {}
 }
